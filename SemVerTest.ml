070d78137e196374b95149ceb7e0eeed5250e8ae
Joseph Stankowicz <joseph.stankowicz@gmail.com>
2014-07-06 14:00:15 -0700

    added comments to tests

--- SemVerTest.ml
+++ SemVerTest.ml
@@ -4,11 +4,13 @@
 "major was selected from type SemVer.semver. It is not visible in the current scope."*)
 open SemVer ;;
 
+(*Test string to semver conversion*)
 let str = "100.200.300" in
 let semVer = SemVer.semver_of_string str in
 assert (semVer.major == 100 && semVer.minor == 200 && semVer.patch == 300) ;
 SemVer.print semVer ;
 
+(*test comparison operators*)
 let sv123 = { major = 1; minor = 2; patch = 3; } in
 let sv223 = { major = 2; minor = 2; patch = 3; } in
 let sv124 = { major = 1; minor = 2; patch = 4; } in
